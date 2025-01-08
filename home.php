c3132f9ff8376d6ac5db1f3cce07b11471efc83f
joao Carmelio <jcg24708@gmail.com>
2025-01-08 16:59:04 -0300

    adicionado recentemente novas atualizações

---
+++ home.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="home.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home page</title>
+</head>
+<body>
+
+<div class="content">
+    <div>
+      <h3>O QUE VOCÊ DESEJA?</h3>
+    </div>
+    <form class="form">
+
+      <button class="bnt"><a href="#">LOGIN</a></button>
+      <br>
+      <button class="bnt"><a href="#">REGISTRAR</a></button>
+      
+      <form>
+  </div>
+    
+</body>
+</html>
